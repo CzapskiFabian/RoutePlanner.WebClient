bc8a8254022585a6fd659a52f697ed5de24fcab0
Fabian Czapski <czapskifabian@gmail.com>
2017-05-10 15:43:02 +0100

    Add Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,27 @@
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ResultsComponent } from './results/results.component';
-import { ServerService } from './shared/integration/server.service';
-import { EngineerService } from './shared/services/engineers.service';
-import { JobsService } from './shared/services/jobs.service';
-import { RequestService } from './shared/services/request.service';
-import { WorkspaceModule } from './workspace/workspace.module';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ResultsComponent } from './results/results.component';
+import { WorkspaceModule } from './workspace/workspace.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { NgModule } from '@angular/core';
 import { AgmCoreModule } from 'angular2-google-maps/core';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     ResultsComponent
   ],
   imports: [
+    CoreModule,
     BrowserModule,
     FormsModule,
-    HttpModule,
     WorkspaceModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBbevaAw-6wYaG_dKtLQdnwzeFzY_sokD8'
-    })
+    }),
   ],
-  providers: [RequestService, ServerService, JobsService, EngineerService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
